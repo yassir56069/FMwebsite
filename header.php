2a0a78abee3b1a731c87931d0bd2625c3dc07720
Vayuna2110 <vayuna.fm@gmail.com>
2023-09-01 10:50:20 +0400

    changes to header

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ session_start();
 <nav class="navbar-container">
     <div class="navigation-bar" >
         <img class="logo" src="assets/FM_logo.png" onclick="window.location.href='home.php'" alt="Fantastic Mind Logo">
-        <ul class="page-link-box">
+        <ul class="page-link-box ">
         <?php
 
             $about_us       = 'about_us.php';
@@ -35,14 +35,12 @@ session_start();
             echo "<a class='page-link' href='{$iot_solution}'>research & design</a>";
             echo '</li>';
         ?>
-
-
         </ul>
-
-        <section class="megamenu">
-                    TEST 
-        </section>
         <button class="o-btn nav-button">Contact Us</button>
     </div>
+    
+    <section class="megamenu">
+            TEST 
+    </section>
 </nav>
 
