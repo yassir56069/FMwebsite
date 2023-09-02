ebed859e9ea50295dcb601f817c51b891085cac6
NORBS06 <anorbert.fm@gmail.com>
2023-09-02 15:50:20 +0400

    iconekt done

---
+++ iconekt.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Iconekt</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+            <!-- navigation-bar -->
+            <div class="ic-header-section">
+                <div class="navigation-bar">
+                    <img class="logo" src="assets/FM_logo.png" onclick="window.location.href='home.php'" alt="Fantastic Mind Logo">
+                    <div class="page-link">
+                        <a href="about_us.php">about us</a> 
+                        <a href="services.php">services</a> 
+                        <a href="iot_solution.php">iot solution</a> 
+                        <a href="r&d_use_case.php">research & design</a>
+                    </div>
+                    <button class="o-btn">Contact Us</button>
+                </div>
+                <div class="header-text">
+                    <h2>
+                        Experience Our  <br />
+                        Unique Approach
+                    </h2>
+                    <p style="text-align: center;">
+                        Pioneering a Journey of Transformation. Seamlessly Weave Data into Decisions. Elevate<br />
+                        Efficiency and Insight. Embrace the Future of Smart Solutions.<br />
+                        Join Us in Shaping Tomorrow's Success
+                    </p>
+                </div>
+            </div>
+
+
+            <!-- Iconekt -->
+            <section class="wsection">
+                <div style="width: 1184px;">
+                <div class="h-container" style="width: 100%; height: 144px;margin-bottom: 24px;display: inline-block;">
+                    <div class="v-container"style="width: 488px; height: 144px;float: left;">
+                        <p class="b-head-text" style="padding-bottom: 24px;text-align: left;font-weight: 700;">
+                            OUR APPROACH
+                        </p>
+                        <p class="body-text" style="height: 60px;">
+                            Embark on a Transformative Journey with Us. Dive into a World of Possibilities as We Unveil How Our 5 steps' Approach Translates into Real Solutions. Discover the Future of Data-driven Success.
+                        </p>
+                    </div>
+                    <img src="assets/iconekt logo (for dark bkg).svg" alt="Iconekt Logo" style="width: 385px;float: right;">
+                </div>
+                <ul class="fbrot">
+                    <li class='rotblock'> 
+                            <img class="rotsvg" src="assets/iconekt_Flashbuild.svg" alt="FlashBuild"> 
+                            <p class='rottext'>
+                                <b>The Define and Discovery Phase</b><br />
+                                Embark on your innovation journey with FlashBuild. As the cornerstone of project onboarding, it's the phase where we collaborate to shape your IoT-powered vision. By following the framework, set the stage for a successful, data-driven future
+                            </p>
+                    </li>
+                    <li class='rotblock'> 
+                            <img  class="rotsvg" src="assets/iconekt_Connect.svg" alt="Connect"> 
+                            <p class='rottext'>
+                                Begin your journey by seamlessly integrating IoT devices and sensors into your new project's operations. The connect process establishes a network that captures real-time data, forging the foundation of your data-driven venture.
+                            </p>
+    
+                    </li>
+                    <li class='rotblock'> 
+                            <img  class="rotsvg" src="assets/iconekt_Collect.svg" alt="Collect"> 
+                            <p class='rottext'>
+                                The collect process encompasses robust and secure data collection infrastructure. It captures, stores, and processes substantial data volumes, setting the stage for meaningful insights that will define your project's success.
+                            </p>
+                    </li>
+                    <li class='rotblock'> 
+                            <img  class="rotsvg" src="assets/iconekt_Analyse.svg" alt="Analyse"> 
+                             <p class='rottext'>
+                                Unleash the power of analysis of dta. A state-of-the-art analytics engine, backed by advanced algorithms and machine learning techniques, transforms raw data into valuable insights. Visualization tools aid exploration of data trends, offering a clear path to informed decision-making.
+                            </p>
+                            
+                        </li>
+                    <li class='rotblock'> 
+                            <img  class="rotsvg" src="assets/iconekt_Act.svg" alt="Act"> 
+                            <p class='rottext'>
+                                Empower your new project with actionable insights through the act process. Real-time alerts, predictive analytics, and prescriptive recommendations guide your initial steps, ensuring intelligent and impactful actions from the outset.
+                            </p>
+                    </li>           
+                                                 
+                </ul> 
+                </div>
+            </section> 
+            <div class="wsection" style="height: 300px;"></div>
+            
+            
+            <!-- Data As A service -->
+            <div class="bsection">
+                <div class="h-container" style="width: 100%;height: 100%;">
+                    <img style="width: 432px; margin-right: 162px;margin-top: 110px;margin-left: 161px;" src="assets/I_Data_Phone.png" alt="phone">
+                    <div class="v-container" style="display: inline-block;width: 543px;">
+                        <img style="width: 542px;margin-top: 151px;float: left;" src="assets/data-as-a-service.svg" alt="data-as-a-service">
+                        <p class="body-text" style="color: #fff;padding-top: 32px;width: 543px;height: 180px;float: left;">
+                            <b>Elevate Your Home Ventures with Iconekt's Innovative DaaS Solutions</b><br />
+                            <br />
+                            Iconekt seamlessly merges state-of-the-art technology, impeccable connectivity, and advanced analytics to reshape your business landscape. Unlock a realm of enhanced efficiency, optimization, and actionable insights that convert your data into invaluable assets. Empower your business with our all-encompassing DaaS solution, leveraging the full potential of the Internet of Things (IoT) to propel your success into the future. Whether you're a new or existing homeowner, or a visionary developer, embrace the power of Iconekt for a transformative journey.
+                        </p>
+                        <button class="g-btn" style="width: 143px;margin-top: 24px;float: right;">Book a demo</button>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Solutions -->
+            <div class="wsection">
+                <div class="v-container" style="width: 1265px;">
+                    <div style="width: 100%; display: inline-block">
+                        <img style="width: 424px; float: left;margin-bottom: 48px;" src="assets/solution.svg" alt="solution">
+                        <div class="h-container" style="width: 100%;height: 332px;display: flex;justify-content: center;">
+
+                            <div class="v-container" style="margin-right: 27px;width: 295px;">
+                                <img style="width: 56px;position: absolute;margin-left: 238px;" src="assets/energy.svg" alt="energy">
+                                <div class="solution-block">
+                                    <p class="ic-head-text">
+                                        energy<br />
+                                        management
+                                    </p>
+                                    <p class="ic-body-text">
+                                        Optimize energy consumption and drive sustainability. Unlock valuable insights into your energy usage, enabling you to make informed decisions & reduce costs.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="v-container" style="margin-right: 27px;width: 295px;">
+                                <img style="width: 56px;position: absolute;margin-left: 238px;" src="assets/water.svg" alt="water">
+                                <div class="solution-block">
+                                    <p class="ic-head-text">
+                                        Water<br />
+                                        management
+                                    </p>
+                                    <p class="ic-body-text">
+                                        Gain real-time visibility into water consumption, detect leaks, and implement conservation strategies, helping you conserve resources.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="v-container" style="margin-right: 27px;width: 295px;">
+                                <img style="width: 56px;position: absolute;margin-left: 238px;" src="assets/security.svg" alt="security">
+                                <div class="solution-block">
+                                    <p class="ic-head-text">
+                                        security<br />
+                                        management
+                                    </p>
+                                    <p class="ic-body-text">
+                                        Secure your assets with confidence. Ensure seamless monitoring and protection of your facilities, providing you with peace of mind and safeguarding your business from potential threats.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="v-container" style="width: 295px;">
+                                <img style="width: 56px;position: absolute;margin-left: 238px;" src="assets/waste.svg" alt="waste">
+                                <div class="solution-block">
+                                    <p class="ic-head-text">
+                                        waste<br />
+                                        management
+                                    </p>
+                                    <p class="ic-body-text">
+                                        Transform waste into opportunity. Our comprehensive system allows you to track waste generation, streamline recycling efforts, and minimize environmental impact.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="b-btn" style="width: 124px;margin-top: 32px;">Contact us</button>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Data as a service -->
+            <div class="bsection">
+                <div class="v-container" style="width: 1136px;">
+                    <img style="width: 900px;" src="assets/data-as-a-service_2.svg" alt="data as a service">
+                    <p class="body-text" style="color: #fff;width: 889px;height: 120px;margin-top: 32px;">
+                        <b>Empowering Industries through Iconekt's Innovative DaaS Solutions</b><br/>
+                        <br/>
+                        Elevate your industrial prowess with Iconekt's fusion of cutting-edge technology, seamless connectivity, and advanced analytics. Embrace an era of heightened efficiency, optimization, and actionable insights as Iconekt magnifies the value of your data. Our comprehensive DaaS solution empowers your business, harnessing IoT's potential to steer your success journey. Whether you're in warehousing, agriculture, or industrial ventures, Iconekt redefines operational excellence.
+                    </p>
+                    <button class="g-btn" style="width: 143px;margin-top: 24px;">Book a demo</button>
+                </div>
+            </div>
+
+
+            <!-- Partners -->
+            <div class="wsection" style="justify-content:center; display: flex;height: 375px;">
+                <div class="v-container">
+                    <p class="b-head-text">
+                        our partners
+                    </p>
+                    <div class="h-container" style="margin-top: 32px;">
+                        <img style=" width:174px; height:59px; margin-right:64px;" src="assets/Sensative.png" alt="sensative-logo">
+
+                        <img style=" width:137px; height:59px;" src="assets/senzemo.png" alt="senzemo-logo">
+                </div>
+                </div>
+            </div>
+
+
+            <!-- Research and Dev -->
+            <!-- Agritech -->
+            <div class="wsection">
+                <div class="v-container" style="width: 1024px;">
+                    <p class="b-head-text" style="font-size: 48px;text-align: left;">
+                        Research and Development Showcase: Innovating the Future
+                    </p>
+                    <p class="body-text" style="width: 841px;height: 20px;padding-top: 48px;padding-bottom: 48px;">
+                        Explore our pioneering use cases that illustrate the potential of IoT in diverse sectors, backed by compelling proof of concept:
+                    </p>
+
+                    <div class="h-container">
+                        <div class="v-container">
+                            <p class="ic-black-head-text">
+                                Agritech: Passion Fruit Plantation in Ferney
+                            </p>
+                            <p class="body-text" style="width: 423px;height: 240px;padding-bottom: 32px;padding-top: 32px;">
+                                Our tailored IoT solution orchestrates precise, data-driven practices. Dive into cultivation's core, monitoring vital aspects like soil composition and moisture. Real-time insights equip farmers to optimize conditions, fine-tune strategies, and boost yields.<br/>
+                                <br/>
+                                Beyond instant gains, innovation champions sustainability. Witness ecology and technology unite, seamlessly. Cultivate sustainability, reducing impact while boosting prosperity. Explore a journey guided by data, from nurturing vines to harvesting fruits. This is agriculture's future – nature and tech, harmoniously intertwined.
+                            </p>
+                            <button class="b-btn" style="width: 151px;">View case study</button>
+                        </div>
+                        <img style="width: 563px;margin-left: 32px;" src="assets/grenadine.png" alt="grenadine">
+                    </div>
+                </div>
+            </div>
+
+            
+            <!-- Residential Excellence -->
+            <div class="wsection">
+                <div class="v-container" style="width: 1024px;">
+                    <div class="h-container">
+                        <img style="width: 563px;margin-right: 117px;margin-top: 30px;" src="assets/Residential Excellence.png" alt="picture">
+                        <div class="v-container" style="margin-top: 54px;">
+                            <p class="ic-black-head-text">
+                                Residential Excellence
+                            </p>
+                            <p class="body-text" style="width: 393px;height: 160px;margin-bottom: 32px;margin-top: 32px;">
+                                Step into a smarter home, where IoT transforms living. Discover our sensors that meticulously track energy consumption, delivering cost-efficient solutions. Experience advanced security management through AI cameras and openings monitoring. Moreover, enjoy optimal comfort with temperature and humidity control. Dive into intelligent pool management with energy-efficient pump usage, real-time chlorine & Ph monitoring, and water level tracking. Energy control?
+                            </p>
+                            <button class="b-btn" style="width: 151px;">View case study</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Insuretech -->
+            <div class="wsection">
+                <div class="v-container" style="width: 1024px;">
+                    <div class="h-container">
+                        <div class="v-container" style="margin-top: 24px;">
+                            <p class="ic-black-head-text">
+                                InsureTech: Revolutionizing Warehousing and Logistics
+                            </p>
+                            <p class="body-text" style="width: 464px;height: 240px;margin-bottom: 32px;margin-top: 32px;">
+                                Delve into the realm of InsureTech through our groundbreaking sensors embedded in warehouses. Experience heightened asset management and precise temperature control in cold and chilled rooms. Unveil a new era in logistics with sensors tracking truck locations and cold room temperatures during transportation.
+                                Our IoT innovation ensures asset security, temperature integrity, and seamless supply chain management.<br/>
+                                <br/>
+                                These proof-of-concept demonstrations showcase the transformative power of IoT. Each scenario emphasizes efficiency, sustainability, and cutting-edge solutions. At the heart of it all lies our commitment to shaping a smarter, more connected future across industries.
+                            </p>
+                            <button class="b-btn" style="width: 151px;">View case study</button>
+                        </div>
+                        <img style="width: 563px;margin-left: 47px;margin-top: 10px;height: 419px;" src="assets/InsureTech-ic.png" alt="worker">
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- footer -->
+            <iframe src="footer.php" width="100%" height="700" frameborder="0" scrolling="no"></iframe>
+    </body>
+</html>
