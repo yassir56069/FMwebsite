36b1acb7db5745d6d8b893aeab6f5afe8e3bb49b
Vayuna2110 <vayuna.fm@gmail.com>
2023-09-04 15:33:54 +0400

    contact us form completed

---
+++ contact_us_submission.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submission</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+        <label class="contact_h3 HT-bold cf-title" style="text-align: center;"> READY TO CONQUER YOUR DIGITAL CHALLENGES? </label><br/>
+
+        <div class="v-container" style="text-align:center;">
+            <img src="assets/Submition.svg " style="margin:auto;"><br/>
+            <label class="contact_h3" style="padding-top: 16px;"> Thank you<br/>for contacting us ! </label>
+            <p class="contact_h3 body-text" style="width: 100%; height:20px;padding-bottom:16px;padding-top: 16px;">
+            We'll get back to you soon.
+            </p>
+            <p class="contact_h3 body-text" style="width: 100%; height:20px;">
+            Meanwhile you can follow us on &nbsp;<a href="https://www.linkedin.com/company/fantastic-mind/" style="color:#1A16E3; font-size:unset;">LinkedIn </a>.
+            </p>
+        </div>
+                 
+
+</body>
+</html>
